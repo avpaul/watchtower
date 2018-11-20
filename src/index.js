8ccc7c091609b931a54029cd97b87972ff0a5df4
Innocent Asiimwe <innocent.asiimwe@andela.com>
2018-11-20 15:18:42 +0300

    chore(bugsnag) integrate bugsnag (#57)
    - add and configure bugsnag
    
    [Finishes #161471993]

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import bugsnag from 'bugsnag-js';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import createPlugin from 'bugsnag-react';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './redux/store/configureStore';
 import Routes from './routes';
 import initialState from './redux/reducers/initialState';
 
+const bugsnagClient = bugsnag(process.env.REACT_APP_BUGSNAG_API_KEY);
+const ErrorBoundary = bugsnagClient.use(createPlugin(React));
 const store = configureStore(initialState);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Routes />
-  </Provider>,
-  document.getElementById('root'),
+  <ErrorBoundary>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
+  </ErrorBoundary>,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
