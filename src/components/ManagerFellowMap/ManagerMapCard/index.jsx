83466c895d0f9b1ae81453b9067faa3be93a3b01
udochukwu <nnaji_udochukwu@yahoo.com>
2019-01-07 11:55:43 +0100

    fix(Managers card): fix text overflow issue (#107)
    -modify the managers card to avoid text overflow
    
    [finishes #162980551]

--- src/components/ManagerFellowMap/ManagerMapCard/index.jsx
+++ src/components/ManagerFellowMap/ManagerMapCard/index.jsx
@@ -26,7 +26,7 @@ const ManagerMapCard = ({ manager }) => {
   return (
     <div className="card manager_card">
       <div className="card-header">
-        <span className="manager_card__name">
+        <span className="manager_card__name float-left" data-toggle="tooltip" data-placement="top" title={ `${manager.firstName}  ${manager.lastName}`}>
           {manager.firstName} {manager.lastName}
         </span>
         <span className="manager_card__fellows--number float-right">
