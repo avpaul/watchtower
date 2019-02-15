ac699b4241e0239c3362a7d6d1c0d2b38c764a90
Adegoke Fred <fredrick.adegoke@gmail.com>
2019-02-15 08:49:08 +0100

    chore(make cards scrollable): (#146)
    - make cards should be scrollable and not overflow to a newline.
    
    [Finishes #163847419]

--- src/views/EngineeringManagerSimsLeadDashboard/TtlsFellowSummary.jsx
+++ src/views/EngineeringManagerSimsLeadDashboard/TtlsFellowSummary.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Slider from 'react-slick';
+import { carouselOptions } from '../../utils';
 import FilterCard from '../../components/Filters/FilterCard';
 import '../../components/FellowsSummary/FellowsSummary.css';
 import FellowSummaryLabel from '../../components/FellowSummaryLabel/FellowSummaryLabel';
@@ -8,21 +10,21 @@ const FellowsSummary = ({ fellowsSummary, handleCardClick }) => (
   <div className="ops-dashboard__fellows-summary">
     <FellowSummaryLabel />
     <div className="row ops-dashboard__filter">
-      {fellowsSummary.map(fellowSummary => (
-        <div className="p-1">
-          <FilterCard
-            key={fellowSummary.id}
-            filterId={fellowSummary.id}
-            cardDetails={fellowSummary}
-            className={
-              fellowSummary.id === 'total-fellows-card'
-                ? 'card total-fellows-card'
-                : 'card'
-            }
-            onClick={handleCardClick}
-          />
-        </div>
-      ))}
+      <div className="scroll-wrapper">
+        <Slider {...carouselOptions(3)}>
+          {fellowsSummary.map(fellowSummary => (
+            <div className="card-scroll-wrapper">
+              <FilterCard
+                key={fellowSummary.id}
+                filterId={fellowSummary.id}
+                cardDetails={fellowSummary}
+                className="card"
+                onClick={handleCardClick}
+              />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   </div>
 );
