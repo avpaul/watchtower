769a1e06151c73f16867fbe5ed36bcdeaed139d1
tesh254 <ewachira254@gmail.com>
2019-03-26 15:51:02 +0300

    bg(alignment fix): table alignment fix
    - render a developers average dev pulse a week
    - fix some of the table alignment issues
    
    [Fixes #164889235]

--- src/components/DevPulseTable/DevPulseRow/DevPulseRow.jsx
+++ src/components/DevPulseTable/DevPulseRow/DevPulseRow.jsx
@@ -7,6 +7,7 @@ import Row from '../../TableComponents/Row';
 const formatRating = rating => {
   const formatedRating = {
     week: rating.week,
+    average: rating.average,
     quantity: rating.quantity,
     quality: rating.quality,
     initiative: rating.quality,
@@ -17,18 +18,55 @@ const formatRating = rating => {
   return formatedRating;
 };
 
-const DevPulseRow = ({ rating }) => (
-  <Row>
-    {Object.keys(formatRating(rating)).map(key => (
-      <Cell
-        key={arrayKey({ key })}
-        addedClass={rating[key] < 1 ? 'text-danger' : null}
-      >
-        {rating[key]}
-      </Cell>
-    ))}
-  </Row>
-);
+/**
+ * @description - Converts whole numbers to floats with two decimal places
+ * e.g. if result is 1, converts to 1.00
+ * @param {*} number
+ * @returns float number
+ */
+const turnWholeNumbersToFloat = number => {
+  // Convert the number to a string
+  let digit = number;
+  digit = digit.toString();
+  digit += digit.includes('.') ? '' : '.00';
+  return digit;
+};
+
+/**
+ * @description - Calculates the average rating for a developer
+ * weekly and returns a float of 2 decimal places. Achieved by
+ * looping through the object passed as a parameter
+ * @param {*} rating
+ * @returns float number
+ */
+const caculateAvg = rating => {
+  let total = 0;
+  Object.keys(formatRating(rating)).map(key => {
+    total += key !== 'week' && key !== 'average' ? parseFloat(rating[key]) : 0;
+    return total;
+  });
+  const value = total / 6;
+  return turnWholeNumbersToFloat(Math.floor(value * 100) / 100);
+};
+
+const DevPulseRow = ({ rating }) => {
+  // calculate avarage
+  const avg = caculateAvg(rating);
+  // assign the average to the rating object
+  Object.assign(rating, { average: avg });
+  return (
+    <Row>
+      {Object.keys(formatRating(rating)).map(key => (
+        <Cell
+          key={arrayKey({ key })}
+          addedClass={rating[key] < 1 ? 'text-danger' : null}
+        >
+          {rating[key]}
+        </Cell>
+      ))}
+    </Row>
+  );
+};
 
 DevPulseRow.propTypes = {
   rating: PropTypes.shape({}).isRequired
