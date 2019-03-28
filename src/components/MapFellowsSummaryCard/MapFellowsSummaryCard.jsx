e65b18c618466fa4e5675e8769aa7eca8ef2e11e
Augustine Ezinwa <augustineezinwa@gmail.com>
2019-03-28 02:49:35 +0100

    bg(fix-status): fix fellow summary card status
    - fix fellow summary card status to display correct fellow status
    - update fellow status display logic
    
    [Fixes #164947230]

--- src/components/MapFellowsSummaryCard/MapFellowsSummaryCard.jsx
+++ src/components/MapFellowsSummaryCard/MapFellowsSummaryCard.jsx
@@ -23,10 +23,15 @@ const resolveStatus = fellow => {
    ** Renders the fellow's current progress status
    * @param fellow Fellow's details as an object
    */
-  if (!fellow.status) return 'No Status';
+  if (!fellow.status && !fellow.pipStatus) return 'No Status';
 
-  if (fellow.status.includes('gteWk5')) return 'PIP';
-  if (fellow.status.includes('ltWk5')) return 'Off-Track';
+  if (
+    fellow.status.includes('gteWk5') ||
+    (fellow.status.includes('ltWk5') && !fellow.pipStatus)
+  )
+    return 'Off-Track';
+
+  if (fellow.pipStatus) return 'PIP';
 
   return 'On-Track';
 };
