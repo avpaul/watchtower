28c768a4434bdf21b37db2b30b725fc7b1dcfcd2
Lazarus Odhiambo <lazarusodhis@gmail.com>
2019-06-20 15:36:49 +0300

    ft (tdd-ops-roles): tdd ops should view all roles  …
    - fetch data from the backend endpoint
    - map the data to the role cards
    - write tests
    
    [Starts #166700762]

---
+++ src/views/CadreViewRoles/CadreViewRoles.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MapProjectRoleCard from '../../components/MapProjectRoleCard';
+import Loader from '../../components/Loader/Loader';
+
+import './CadreViewRoles.css';
+
+export default class CadreViewRoles extends Component {
+  componentDidMount() {
+    const { fetchAllRoles } = this.props;
+    fetchAllRoles();
+  }
+
+  render() {
+    const {
+      allRoles: { data, loading }
+    } = this.props;
+    return loading ? (
+      <div className="role-dashboard__loader">
+        <Loader />
+      </div>
+    ) : (
+      <div>
+        <MapProjectRoleCard roleData={data} />
+      </div>
+    );
+  }
+}
+
+CadreViewRoles.propTypes = {
+  allRoles: PropTypes.shape(),
+  fetchAllRoles: PropTypes.func.isRequired
+};
+
+CadreViewRoles.defaultProps = {
+  allRoles: {}
+};
