1009ae706d158db7402767504ec78f848b9c5d28
Trust Birungi <diamondtrust66@gmail.com>
2019-09-01 21:04:30 +0300

    bug(amplitude):commented out the code that adds amplitude temporarily so as to enable the failing deployment (#514)

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,21 +1,25 @@
 import ReactGA from 'react-ga';
-import AmplitudeAnalytics from 'amplitude-js';
+// import AmplitudeAnalytics from 'amplitude-js';
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID, {
   debug: process.env.NODE_ENV === 'development',
   testMode: process.env.NODE_ENV === 'test'
 });
 
+/*
 AmplitudeAnalytics.getInstance().init(
   process.env.REACT_APP_AMPLITUDE_ANALYTICS_ID
 );
+*/
 
 const activatePageView = ({ pathname, search }) => {
   ReactGA.set({ page: pathname });
   ReactGA.pageview(pathname + search);
 
+  /*
   const { host } = window.location;
   AmplitudeAnalytics.getInstance().logEvent(pathname + search, { host });
+*/
 };
 
 const fireloginEvent = id => {
